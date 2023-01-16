2b06c337651e7722484c7c12b2e1f431cb6fd352
KychataOlena <106249466+KychataOlena@users.noreply.github.com>
2023-01-16 21:45:58 +0200

    app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import { Form } from './Form/Form';
+
+export class App extends Component {
+  // state = {
+  //   contacts: [],
+  //   name: '',
+  // };
+
+  formSubmitHandler = data => {
+    console.log(data);
+  };
+
+  render() {
+    return <Form onSubmit={this.formSubmitHandler} />;
+  }
+}
+
+export default App;
